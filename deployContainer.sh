c078c2910f120765e37ae0aec8d4f61b45d15816
toast <toast@toast-server.net>
2023-09-18 17:34:22 +1000

    Moved host

--- deployContainer.sh
+++ deployContainer.sh
@@ -2,7 +2,7 @@
 # Remotes into production server and deploys the container.
 
 echo "Deploying container to production server..."
-ssh ovhvps "cd Bubbles && docker compose pull && docker compose up -d && exit"
+ssh ovhdocker "cd Bubbles && docker compose pull && docker compose up -d && exit"
 echo "Done."
 
 ###EOF###
