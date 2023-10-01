b7c71abaf91c2be898221d97c8a6b7c0998b5026
toast <toast@toast-server.net>
2023-10-01 17:02:16 +1100

    Not needed as it's automated now.

--- deployContainer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Remotes into production server and deploys the container.
-
-echo "Deploying container to production server..."
-ssh ovhdocker "cd Bubbles && docker compose pull && docker compose up -d && exit"
-echo "Done."
-
-###EOF###
